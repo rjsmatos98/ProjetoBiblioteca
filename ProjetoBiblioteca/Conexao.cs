798da8d8fe025cc48c27257338a28e69748ea4d9
Raimundo Santos <rjsmatos98@gmail.com>
2019-04-22 23:20:22 -0300

    Conexão com banco de dados

---
+++ ProjetoBiblioteca/Conexao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoBiblioteca
+{
+    class Conexao
+    {
+        SqlConnection con;
+        public SqlConnection AbrirConexao()
+        {
+            con = new SqlConnection
+            {
+                ConnectionString = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=E:\Dados Pessoais\Desktop\Udemy\C# Essencial\Biblioteca\ProjetoBiblioteca\ProjetoBiblioteca\bdbiblioteca.mdf;Integrated Security=True;Connect Timeout=30"
+            };
+            con.Open();
+            return con;
+        }
+
+        public void FecharConexao(SqlConnection con)
+        {
+            con.Close();
+        }
+
+    }
+}
